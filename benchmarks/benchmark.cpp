9078fce853b8963fd820089f444a2c5b07daf92b
mkonnerth <matkonnerth@gmail.com>
2020-05-31 21:41:25 +0200

    add benchmarks

---
+++ benchmarks/benchmark.cpp
@@ -0,0 +1,48 @@
+#include <benchmark/benchmark.h>
+#include <BlockAllocator.h>
+#include <string.h>
+
+void createObjects_Calloc(size_t size, size_t objSize)
+{
+    for(size_t cnt = 0; cnt < size; cnt++)
+    {
+        void *mem = calloc(1, objSize);
+        benchmark::DoNotOptimize(mem);
+    }
+    //we know that mem is lost in this solution
+    //freeing memory here up would also take time, so in praxis BlockAlloc performs even better
+}
+
+void createObjects_BlockAlloc(size_t size, size_t objSize)
+{
+    mem::BlockAllocator a(102400);
+    for (size_t cnt = 0; cnt < size; cnt++)
+    {
+        void *mem = a.alloc(objSize);
+        benchmark::DoNotOptimize(mem);
+    }
+}
+
+static void BM_BlockAllocator(benchmark::State &state)
+{
+    
+    // Perform setup here
+    for (auto _ : state)
+    {
+        createObjects_BlockAlloc(state.range(0), 16);
+    }
+}
+
+static void BM_Calloc(benchmark::State &state)
+{
+    // Perform setup here
+    for (auto _ : state)
+    {
+        createObjects_Calloc(state.range(0), 16);
+    }
+}
+// Register the function as a benchmark
+BENCHMARK(BM_Calloc)->Range(100, 1000*1000);
+BENCHMARK(BM_BlockAllocator)->Range(100, 1000 * 1000);
+// Run the benchmark
+BENCHMARK_MAIN();
--- tests/allocator.cpp
+++ tests/allocator.cpp
@@ -1,11 +1,12 @@
-#include <gtest/gtest.h>
-#include <Mem_BlockAllocator.h>
-#include <string.h>
 #include <BlockAllocator.h>
+#include <Mem_BlockAllocator.h>
+#include <gtest/gtest.h>
+#include <string.h>
 
-TEST(Allocator, basics) {
-    Mem_BlockAllocator* a = Mem_BlockAllocator_new(1024);
-    void* mem = Mem_BlockAllocator_malloc(a, 1024);
+TEST(Allocator, basics)
+{
+    Mem_BlockAllocator *a = Mem_BlockAllocator_new(1024);
+    void *mem = Mem_BlockAllocator_malloc(a, 1024);
     memset(mem, 99, 1024);
     Mem_BlockAllocator_delete(a);
 }
@@ -15,4 +16,43 @@ TEST(Allocator, highLevel)
     mem::BlockAllocator a(1024);
     void *mem = a.alloc(1024);
     memset(mem, 99, 1024);
+}
+
+TEST(Allocator, bigAlloc)
+{
+    mem::BlockAllocator a(100);
+    void *mem = a.alloc(1024 * 1024);
+    memset(mem, 99, 1024 * 1024);
+}
+
+TEST(Allocator, memcmp)
+{
+    mem::BlockAllocator a(100);
+    char cmp1[100];
+    memset(cmp1, 1, 100);
+    char *mem = static_cast<char *>(a.alloc(100));
+    memcpy(mem, cmp1, 100);
+    char cmp2[100];
+    char *mem2 = static_cast<char *>(a.alloc(100));
+    memset(cmp2, 2, 100);
+    memcpy(mem2, cmp2, 100);
+
+    ASSERT_EQ(memcmp(mem, cmp1, 100), 0);
+    ASSERT_EQ(memcmp(mem2, cmp2, 100), 0);
+}
+
+TEST(Allocator, realloc)
+{
+    mem::BlockAllocator a(100);
+    char cmp1[100];
+    memset(cmp1, 1, 100);
+    char *mem = static_cast<char *>(a.alloc(100));
+    memcpy(mem, cmp1, 100);
+    char cmp2[100];
+    char *mem2 = static_cast<char *>(a.realloc(200));
+    memset(cmp2, 2, 100);
+    memcpy(mem2+100, cmp2, 100);
+
+    ASSERT_EQ(memcmp(mem2, cmp1, 100), 0);
+    ASSERT_EQ(memcmp(mem2+100, cmp2, 100), 0);
 }
